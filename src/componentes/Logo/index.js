b67c361e0f0805546589c90b87ebfb74c066aa87
adriano barbosa <adrianobarbosa_4@hotmail.com>
2023-11-07 17:57:07 -0300

    commit inicial

---
+++ src/componentes/Logo/index.js
@@ -0,0 +1,20 @@
+import logo from '../../imagens/logo.svg'
+import styled from 'styled-components'
+
+const LogoContainer = styled.div`
+    display: flex;
+    font-size: 30px;
+`    
+const LogoImagem = styled.img`
+    margin-right: 10px;
+`
+function Logo() {
+    return(
+        <LogoContainer>
+            <LogoImagem src={logo} alt='logo'/>
+            <p><strong>Adr</strong>Books</p>
+      </LogoContainer>
+    )
+}
+
+export default Logo
---
+++ src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -0,0 +1,9 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    
+    {nome: 'JavaScript Assertivo', id: 1, src: livro},
+    {nome: 'Cangaceiro em React: Uma aventura no sertão da programação', id: 2, src: livro},
+    {nome: 'Apache Java com Spring Boot', id: 3, src: livro},
+
+]
